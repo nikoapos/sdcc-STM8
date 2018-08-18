8d0cd2aa5bbadc0359eef94efb0bdf5fea261735
Nikolaos Apostolakos <nikoapos@gmail.com>
2018-08-18 15:53:49 +0200

    Add the copyright notice to the simple_gpio_example.c

--- src/simple_gpio_example.c
+++ src/simple_gpio_example.c
@@ -1,4 +1,19 @@
-#include <gpio.h>
+/*
+ * Copyright (C) 2018 Nikolaos Apostolakos <nikoapos@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /*
  * This example demonstrates very simple use of the GPIOs as input and outputs.
@@ -25,6 +40,8 @@
  * - Connect the one side of the switch to the C5 pin
  * - Connect the other side of the switch to the 3V3
  */
+
+#include <gpio.h>
 
 // Define the port and pins where the LED and the switch are connected. This is
 // optional. The values can be passed directly to the macro functions
