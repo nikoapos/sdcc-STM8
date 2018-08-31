73b6616fa1381dc5638a19b294a6f15a1e6d7076
Nikolaos Apostolakos <nikoapos@gmail.com>
2018-08-31 18:39:36 +0200

    Add speed measurement in the WheelSpeedReader

--- src/programs/WheelSpeedReader.c
+++ src/programs/WheelSpeedReader.c
@@ -29,12 +29,30 @@
  * turn, the related counter is increased twice as the number of the encoder
  * disc cuts. These counters can be accessed via the I2C registers 0x01-0x04.
  * 
+ * The controller uses the counters to compute the number of encoder disc cuts
+ * per second. The frequency this computation is performed can be controlled
+ * for each wheel individually via the registers 0xA1-0xA4. These registers can
+ * be written with the period of the measurement in ms for each counter (the
+ * default value is 100ms). This value can be tuned based on the speed of the
+ * wheels. The longer the period the more accurate the measurement. The shorter
+ * the period, more frequent updates. For example, if the wheel turns very slow
+ * (once per second) this period should be significantly increased. The measured
+ * frequency can be accessed via the I2C registers 0x11-0x14.
+ * 
  * I2C registers:
  * 
  * - 0x01 (uint16_t) : Counter 1
  * - 0x02 (uint16_t) : Counter 2
  * - 0x03 (uint16_t) : Counter 3
  * - 0x04 (uint16_t) : Counter 4
+ * - 0x11 (float 4 byte) : Counter 1 speed (in counts/sec)
+ * - 0x12 (float 4 byte) : Counter 2 speed (in counts/sec)
+ * - 0x13 (float 4 byte) : Counter 3 speed (in counts/sec)
+ * - 0x14 (float 4 byte) : Counter 4 speed (in counts/sec)
+ * - 0xA1 (uint16_t) : Speed measure period 1
+ * - 0xA2 (uint16_t) : Speed measure period 2
+ * - 0xA3 (uint16_t) : Speed measure period 3
+ * - 0xA4 (uint16_t) : Speed measure period 4
  */
 
 #include <stdbool.h>
@@ -42,6 +60,7 @@
 #include <i2c.h>
 #include <gpio.h>
 #include <itc.h>
+#include <tim4.h>
 
 // The slave I2C address the micro controller will listen to
 #define I2C_ADDRESS 0x55
@@ -53,42 +72,24 @@
 #define PIN_IN_3 5
 #define PIN_IN_4 6
 
-// The counters for the four inputs
-uint16_t count_1 = 0;
-uint16_t count_2 = 0;
-uint16_t count_3 = 0;
-uint16_t count_4 = 0;
+typedef struct {
+  uint16_t count; // The counter of the wheel
+  bool state; // The current state of the photo-interrupter
+  uint16_t period; // The period in ms to perform a speed measurement
+  uint16_t last_meas_time; // The ms passed from the last measurement time
+  float counts_speed; // The counter speed in counts/sec
+  uint16_t last_count; // The value of the counter during the last measurement
+} Wheel;
 
-// The current states of the inputs
-bool state_1 = false;
-bool state_2 = false;
-bool state_3 = false;
-bool state_4 = false;
+Wheel wheel_1;
+Wheel wheel_2;
+Wheel wheel_3;
+Wheel wheel_4;
 
-// Updates the counters if the state of the input has changed
-void updateCounters() {
-  // Read the new state of the inputs
-  bool new_state_1 = GPIO_READ_INPUT(PORT_IN, PIN_IN_1);
-  bool new_state_2 = GPIO_READ_INPUT(PORT_IN, PIN_IN_2);
-  bool new_state_3 = GPIO_READ_INPUT(PORT_IN, PIN_IN_3);
-  bool new_state_4 = GPIO_READ_INPUT(PORT_IN, PIN_IN_4);
-
-  // Update the counters
-  if (new_state_1 != state_1) {
-    ++count_1;
-    state_1 = new_state_1;
-  }
-  if (new_state_2 != state_2) {
-    ++count_2;
-    state_2 = new_state_2;
-  }
-  if (new_state_3 != state_3) {
-    ++count_3;
-    state_3 = new_state_3;
-  }
-  if (new_state_4 != state_4) {
-    ++count_4;
-    state_4 = new_state_4;
+void updateWheelCounter(Wheel* wheel, bool new_state) {
+  if (new_state != wheel->state) {
+    wheel->count += 1;
+    wheel->state = new_state;
   }
 }
 
@@ -97,6 +98,23 @@ int main() {
   
   // Set the f_master to 16 MHz
   CLK_SET_HSI_DIVIDER(1);
+  
+  // Set the TIM4 timer to create an interrupt every 1ms
+  // We run at 16 Mz and we set the prescaler to 128, so the frequency of the
+  // TIM4 is 125 kHz
+  TIM4_SET_PRESCALER(TIM4_PRESCALER_128);
+  // We want 1 ms period, so the timer should overflow when it reaches 124
+  TIM4_SET_PERIOD(124);
+  
+  // Set the period of all the wheels to 100ms (the default)
+  wheel_1.period = 100;
+  wheel_2.period = 100;
+  wheel_3.period = 100;
+  wheel_4.period = 100;
+  
+  // Enable the TIM4 interrupts and start it
+  TIM4_ENABLE_INTERRUPT();
+  TIM4_START();
   
   // Reset all pins to use the minimum power
   GPIO_SET_ALL_PORTS_INPUT_PULL_UP_NO_INT();
@@ -112,11 +130,16 @@ int main() {
   
   // Enable the interrupts
   ITC_SET_PRIORITY(ITC_IRQ_I2C, 3); // I2C must have the highest priority
+  ITC_SET_PRIORITY(ITC_IRQ_TIM4_UPD_OVF, 2);
   enableInterrupts();
   
   // Start an infinite loop which updates the counters constantly
   while(1) {
-    updateCounters();
+    // Update the counters
+    updateWheelCounter(&wheel_1, GPIO_READ_INPUT(PORT_IN, PIN_IN_1));
+    updateWheelCounter(&wheel_2, GPIO_READ_INPUT(PORT_IN, PIN_IN_2));
+    updateWheelCounter(&wheel_3, GPIO_READ_INPUT(PORT_IN, PIN_IN_3));
+    updateWheelCounter(&wheel_4, GPIO_READ_INPUT(PORT_IN, PIN_IN_4));
   }
   
 }
@@ -124,22 +147,79 @@ int main() {
 // This method controls the I2C register address each internal variable is
 // exposed to
 uint8_t* getMemoryPointer(uint8_t id, uint8_t* size) {
-  switch (id) {
+  // The first 4 bits of the address indicate the variable
+  uint8_t var_id = id & 0xF0;
+  // The last 4 bits of the address indicate the wheel
+  uint8_t wheel_id = id & 0x0F;
+  Wheel* wheel;
+  
+  switch (wheel_id) {
     case 0x01:
-      *size = 2;
-      return &count_1;
+      wheel = &wheel_1;
+      break;
     case 0x02:
-      *size = 2;
-      return &count_2;
+      wheel = &wheel_2;
+      break;
     case 0x03:
-      *size = 2;
-      return &count_3;
+      wheel = &wheel_3;
+      break;
     case 0x04:
-      *size = 2;
-      return &count_4;
+      wheel = &wheel_4;
+      break;
+    default:
+      return 0;
   }
+  
+  switch (var_id) {
+    case 0x00:
+      *size = 2;
+      return &(wheel->count);
+    case 0xA0:
+      *size = 2;
+      return &(wheel->period);
+    case 0x10:
+      *size = 4;
+      return &(wheel->counts_speed);
+  }
+  
   return 0;
 }
 
 // Setup the I2C interruption to expose the variables in the memory
 i2cMemorySlaveIterruptHandler(getMemoryPointer)
+
+
+void measureSpeed(Wheel* wheel) {
+  // Check if we have an overflow of the wheel counter
+  if (wheel->last_count > wheel->count) {
+    // We don't do a measurement a this time and we setup the variables to
+    // restart the measurement period
+    wheel->last_count = wheel->count;
+    wheel->last_meas_time = 0;
+    return;
+  }
+  
+  // Increase the time from last measurement by 1ms and check if we need to
+  // perform a measurement
+  wheel->last_meas_time += 1;
+  if (wheel->last_meas_time < wheel->period) {
+    return;
+  }
+  
+  // Compute the counts per second
+  wheel->counts_speed = 1000. * (wheel->count - wheel->last_count) / wheel->period;
+  
+  // Restart the measurement period
+  wheel->last_count = wheel->count;
+  wheel->last_meas_time = 0;
+  
+}
+
+// Called every time the TIM4 overflows, aka every 1ms
+void measureSpeedEvent() __interrupt(ITC_IRQ_TIM4_UPD_OVF) {
+  // First we clear the interrupt flag
+  TIM4_CLEAR_UPDATE_INTERRUPT_FLAG();
+  
+  // Measure the speeds of all wheels
+  measureSpeed(&wheel_1);
+}
