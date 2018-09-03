912750825fae62a36088c23a525874921f0e18a8
Nikolaos Apostolakos <nikoapos@gmail.com>
2018-09-03 12:39:54 +0200

    Realign the i2c.h for better readability

--- include/i2c.h
+++ include/i2c.h
@@ -32,53 +32,53 @@
 ///////////////////////////////////////////////////////////////////////////////
 // I2C related registers
 ///////////////////////////////////////////////////////////////////////////////
-#define REGISTER_I2C_CR1 REGISTER 0x5210 // I2C control register 1
-#define REGISTER_I2C_CR2 REGISTER 0x5211 // I2C control register 2
-#define REGISTER_I2C_FREQR REGISTER 0x5212 // I2C frequency register
-#define REGISTER_I2C_OARL REGISTER 0x5213 // I2C own address register low
-#define REGISTER_I2C_OARH REGISTER 0x5214 // I2C own address register high
-#define REGISTER_I2C_DR REGISTER 0x5216 // I2C data register
-#define REGISTER_I2C_SR1 REGISTER 0x5217 // I2C status register 1
-#define REGISTER_I2C_SR2 REGISTER 0x5218 // I2C status register 2
-#define REGISTER_I2C_SR3 REGISTER 0x5219 // I2C status register 3 
-#define REGISTER_I2C_ITR REGISTER 0x521A // I2C interrup control register
-#define REGISTER_I2C_CCRL REGISTER 0x521B // I2C clock control register low
-#define REGISTER_I2C_CCRH REGISTER 0x521C // I2C clock control register high
+#define REGISTER_I2C_CR1 REGISTER    0x5210 // I2C control register 1
+#define REGISTER_I2C_CR2 REGISTER    0x5211 // I2C control register 2
+#define REGISTER_I2C_FREQR REGISTER  0x5212 // I2C frequency register
+#define REGISTER_I2C_OARL REGISTER   0x5213 // I2C own address register low
+#define REGISTER_I2C_OARH REGISTER   0x5214 // I2C own address register high
+#define REGISTER_I2C_DR REGISTER     0x5216 // I2C data register
+#define REGISTER_I2C_SR1 REGISTER    0x5217 // I2C status register 1
+#define REGISTER_I2C_SR2 REGISTER    0x5218 // I2C status register 2
+#define REGISTER_I2C_SR3 REGISTER    0x5219 // I2C status register 3 
+#define REGISTER_I2C_ITR REGISTER    0x521A // I2C interrup control register
+#define REGISTER_I2C_CCRL REGISTER   0x521B // I2C clock control register low
+#define REGISTER_I2C_CCRH REGISTER   0x521C // I2C clock control register high
 #define REGISTER_I2C_TRISER REGISTER 0x521D // I2C TRISE register
-#define REGISTER_I2C_PECR REGISTER 0x521E // I2C packet error checking register
+#define REGISTER_I2C_PECR REGISTER   0x521E // I2C packet error checking register
 
 ///////////////////////////////////////////////////////////////////////////////
 // I2C register flags
 ///////////////////////////////////////////////////////////////////////////////
 #define I2C_CR1_NOO_STRETCH (uint8_t) 0b10000000
-#define I2C_CR1_ENGC (uint8_t) 0b01000000
-#define I2C_CR1_PE (uint8_t) 0b00000001
-#define I2C_CR2_ACK (uint8_t) 0b00000100
-#define I2C_CR2_STOP (uint8_t) 0b00000010
-#define I2C_CR2_START (uint8_t) 0b00000001
-#define I2C_SR1_TXE (uint8_t) 0b10000000
-#define I2C_SR1_RXNE (uint8_t) 0b01000000
-#define I2C_SR1_STOPF (uint8_t) 0b00010000
-#define I2C_SR1_ADD10 (uint8_t) 0b00001000
-#define I2C_SR1_BTF (uint8_t) 0b00000100
-#define I2C_SR1_ADDR (uint8_t) 0b00000010
-#define I2C_SR1_SB (uint8_t) 0b00000001
-#define I2C_SR2_WUFH (uint8_t) 0b00100000
-#define I2C_SR2_OVR (uint8_t) 0b00001000
-#define I2C_SR2_AF (uint8_t) 0b00000100
-#define I2C_SR2_ARLO (uint8_t) 0b00000010
-#define I2C_SR2_BERR (uint8_t) 0b00000001
-#define I2C_SR3_GENCALL (uint8_t) 0b00010000
-#define I2C_SR3_TRA (uint8_t) 0b00000100
-#define I2C_SR3_BUSY (uint8_t) 0b00000010
-#define I2C_SR3_MSL (uint8_t) 0b00000001
-#define I2C_OARH_ADDMODE (uint8_t) 0b10000000
-#define I2C_OARH_ADDCONF (uint8_t) 0b01000000
-#define I2C_CCRH_FS (uint8_t) 0b10000000
-#define I2C_CCRH_DUTY (uint8_t) 0b01000000
-#define I2C_ITR_ITBUFEN (uint8_t) 0b00000100
-#define I2C_ITR_ITEVTEN (uint8_t) 0b00000010
-#define I2C_ITR_ITERREN (uint8_t) 0b00000001
+#define I2C_CR1_ENGC        (uint8_t) 0b01000000
+#define I2C_CR1_PE          (uint8_t) 0b00000001
+#define I2C_CR2_ACK         (uint8_t) 0b00000100
+#define I2C_CR2_STOP        (uint8_t) 0b00000010
+#define I2C_CR2_START       (uint8_t) 0b00000001
+#define I2C_SR1_TXE         (uint8_t) 0b10000000
+#define I2C_SR1_RXNE        (uint8_t) 0b01000000
+#define I2C_SR1_STOPF       (uint8_t) 0b00010000
+#define I2C_SR1_ADD10       (uint8_t) 0b00001000
+#define I2C_SR1_BTF         (uint8_t) 0b00000100
+#define I2C_SR1_ADDR        (uint8_t) 0b00000010
+#define I2C_SR1_SB          (uint8_t) 0b00000001
+#define I2C_SR2_WUFH        (uint8_t) 0b00100000
+#define I2C_SR2_OVR         (uint8_t) 0b00001000
+#define I2C_SR2_AF          (uint8_t) 0b00000100
+#define I2C_SR2_ARLO        (uint8_t) 0b00000010
+#define I2C_SR2_BERR        (uint8_t) 0b00000001
+#define I2C_SR3_GENCALL     (uint8_t) 0b00010000
+#define I2C_SR3_TRA         (uint8_t) 0b00000100
+#define I2C_SR3_BUSY        (uint8_t) 0b00000010
+#define I2C_SR3_MSL         (uint8_t) 0b00000001
+#define I2C_OARH_ADDMODE    (uint8_t) 0b10000000
+#define I2C_OARH_ADDCONF    (uint8_t) 0b01000000
+#define I2C_CCRH_FS         (uint8_t) 0b10000000
+#define I2C_CCRH_DUTY       (uint8_t) 0b01000000
+#define I2C_ITR_ITBUFEN     (uint8_t) 0b00000100
+#define I2C_ITR_ITEVTEN     (uint8_t) 0b00000010
+#define I2C_ITR_ITERREN     (uint8_t) 0b00000001
 
 
 ///////////////////////////////////////////////////////////////////////////////
